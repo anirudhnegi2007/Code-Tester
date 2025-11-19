8c9d4588fc1fb21fa58b900d69f5c130a65260af
anirudhnegi2007 <anirudhnegi2007@gmail.com>
2025-11-19 19:29:37 +0530

    comment  frontend serve in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,9 +14,9 @@ app.get("/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
-  });
+  // app.get("/{*any}", (req, res) => {
+  //   res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  // });
 }
 
 const startServer = async () => {
